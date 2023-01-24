4350db3a2dfc135b80cb7496e10a57bf2935edde
muriloalvesdev <murilo.alves@picpay.com>
2023-01-24 01:01:11 -0300

    constants for tests

---
+++ src/test/java/br/com/transaction/ConstantsTests.java
@@ -0,0 +1,17 @@
+package br.com.transaction;
+
+import br.com.transaction.dataprovider.database.entity.OperationsType;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public interface ConstantsTests {
+    String DOCUMENT_NUMBER = "12345678900";
+    UUID ACCOUNT_ID = UUID.randomUUID();
+    OperationsType PAGAMENTO = OperationsType.PAGAMENTO;
+    OperationsType COMPRA_A_VISTA = OperationsType.COMPRA_A_VISTA;
+    OperationsType COMPRA_PARCELADA = OperationsType.COMPRA_PARCELADA;
+    OperationsType SAQUE = OperationsType.SAQUE;
+    BigDecimal AMOUNT_POSITIVE = BigDecimal.valueOf(100.00);
+    BigDecimal AMOUNT_NEGATIVE = AMOUNT_POSITIVE.negate();
+}
