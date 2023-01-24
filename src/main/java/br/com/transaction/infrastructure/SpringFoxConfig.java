94f56cd81d61e4eb4c60070fd6ab48d93d879917
muriloalvesdev <murilo.alves@picpay.com>
2023-01-24 19:15:06 -0300

    improving access api documentation

--- src/main/java/br/com/transaction/infrastructure/SpringFoxConfig.java
+++ src/main/java/br/com/transaction/infrastructure/SpringFoxConfig.java
@@ -1,7 +1,10 @@
 package br.com.transaction.infrastructure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
+
+    @Value("${swaggerhub.documentation.url}")
+    private String documentationUrl;
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", this.documentationUrl);
+        registry.addRedirectViewController("/swagger-ui/index.html", this.documentationUrl);
+    }
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
