8f0217e38058b2079c65c359f159ab78b9f2b9c3
muriloalvesdev <murilo.alves@picpay.com>
2023-01-24 01:00:48 -0300

    usecase implementations

---
+++ src/main/java/br/com/transaction/core/usecase/impl/UseCaseAccountImpl.java
@@ -0,0 +1,37 @@
+package br.com.transaction.core.usecase.impl;
+
+import br.com.transaction.core.exception.AccountNotFoundException;
+import br.com.transaction.core.exception.AlreadyDocumentNumberException;
+import br.com.transaction.core.gateway.AccountGateway;
+import br.com.transaction.core.usecase.UseCaseAccount;
+import br.com.transaction.dataprovider.database.entity.Account;
+import br.com.transaction.entriypoint.dto.AccountDto;
+
+import java.util.UUID;
+
+public class UseCaseAccountImpl implements UseCaseAccount {
+
+    private final AccountGateway gateway;
+
+    public UseCaseAccountImpl(final AccountGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public UUID save(final AccountDto dto) {
+        if (this.gateway.existsByDocumentNumber(dto.getDocumentNumber()))
+            throw new AlreadyDocumentNumberException(dto.getDocumentNumber());
+
+        final Account entity = Account.builder()
+            .documentNumber(dto.getDocumentNumber())
+            .build();
+
+        return this.gateway.save(entity).getUuid();
+    }
+
+    public AccountDto find(final UUID accountId) {
+        return this.gateway.find(accountId)
+            .map(AccountDto::build)
+            .orElseThrow(() -> new AccountNotFoundException(accountId));
+    }
+
+}
---
+++ src/main/java/br/com/transaction/core/usecase/impl/UseCaseTransactionImpl.java
@@ -0,0 +1,68 @@
+package br.com.transaction.core.usecase.impl;
+
+import br.com.transaction.core.exception.AccountNotFoundException;
+import br.com.transaction.core.exception.InvalidAmountException;
+import br.com.transaction.core.gateway.AccountGateway;
+import br.com.transaction.core.gateway.TransactionGateway;
+import br.com.transaction.core.usecase.UseCaseTransaction;
+import br.com.transaction.dataprovider.database.entity.Account;
+import br.com.transaction.dataprovider.database.entity.OperationsType;
+import br.com.transaction.dataprovider.database.entity.Transaction;
+import br.com.transaction.entriypoint.dto.TransactionDto;
+
+import java.util.UUID;
+
+public class UseCaseTransactionImpl implements UseCaseTransaction {
+
+    private final TransactionGateway transactionGateway;
+    private final AccountGateway accountGateway;
+
+    public UseCaseTransactionImpl(
+        final TransactionGateway transactionGateway,
+        final AccountGateway accountGateway) {
+        this.transactionGateway = transactionGateway;
+        this.accountGateway = accountGateway;
+    }
+
+    public UUID save(final TransactionDto dto) {
+        final OperationsType type = OperationsType.fromString(dto.getOperationType());
+
+        final Transaction.TransactionBuilder builder = Transaction.builder()
+            .type(type);
+
+        setAmount(dto, type, builder);
+
+        final Account accountEntity = getAccountEntity(dto);
+
+        final Transaction transactionEntity = builder.account(accountEntity).build();
+
+        return this.transactionGateway.save(transactionEntity).getUuid();
+    }
+
+    private void setAmount(
+        final TransactionDto transactionDto,
+        final OperationsType type,
+        final Transaction.TransactionBuilder builder) {
+
+        if (amountInvalid(transactionDto, type)) {
+            throw new InvalidAmountException(type);
+        }
+
+        final var amount = type.convertAmount(transactionDto.getAmount());
+        builder.amount(amount);
+    }
+
+    private boolean amountInvalid(
+        final TransactionDto transactionDto,
+        final OperationsType type) {
+        return transactionDto.isAmountPositive() && type.isNegative() ||
+            transactionDto.isAmountNegative() && type.isPositive();
+    }
+
+    private Account getAccountEntity(
+        final TransactionDto dto) {
+        return this.accountGateway.find(dto.getAccountId())
+            .orElseThrow(() -> new AccountNotFoundException(dto.getAccountId()));
+    }
+
+}
