e40a7b669d000a05084a324afdd4ac709b67a3dd
muriloalvesdev <murilo.alves@picpay.com>
2023-01-24 19:15:16 -0300

    add message when init tests

--- .ci/integration-test.sh
+++ .ci/integration-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx1G -Xms128m"
+echo "--- Starting integrated tests now ---"
 mvn package -Dgroups="integration-test"
--- .ci/unit-test.sh
+++ .ci/unit-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx1G -Xms128m"
+echo "--- Starting unit tests now ---"
 mvn package -Dgroups="unit-test"
