07634cccb4fab6df3f47b53e19c74a13dec541a7
muriloalvesdev <murilo.alves@picpay.com>
2023-01-27 19:33:06 -0300

    pair - beans

--- src/main/java/br/com/transaction/infrastructure/ApplicationConfig.java
+++ src/main/java/br/com/transaction/infrastructure/ApplicationConfig.java
@@ -1,14 +1,17 @@
 package br.com.transaction.infrastructure;
 
 import br.com.transaction.core.gateway.AccountGateway;
+import br.com.transaction.core.gateway.BalanceGateway;
 import br.com.transaction.core.gateway.TransactionGateway;
 import br.com.transaction.core.usecase.account.UseCaseAccount;
-import br.com.transaction.core.usecase.transaction.UseCaseTransaction;
 import br.com.transaction.core.usecase.account.UseCaseAccountImpl;
+import br.com.transaction.core.usecase.transaction.UseCaseTransaction;
 import br.com.transaction.core.usecase.transaction.UseCaseTransactionImpl;
 import br.com.transaction.dataprovider.database.gateway.AccountGatewayImpl;
+import br.com.transaction.dataprovider.database.gateway.BalanceGatewayImpl;
 import br.com.transaction.dataprovider.database.gateway.TransactionGatewayImpl;
 import br.com.transaction.dataprovider.database.repository.AccountRepository;
+import br.com.transaction.dataprovider.database.repository.BalanceRepository;
 import br.com.transaction.dataprovider.database.repository.TransactionRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -20,9 +23,21 @@ public class ApplicationConfig {
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
 
-    ApplicationConfig(final AccountRepository accountRepository, final TransactionRepository transactionRepository) {
+    private final BalanceRepository balanceRepository;
+
+    ApplicationConfig(
+        final AccountRepository accountRepository,
+        final TransactionRepository transactionRepository,
+        final BalanceRepository balanceRepository) {
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
+        this.balanceRepository = balanceRepository;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(BalanceGateway.class)
+    public BalanceGateway balanceGateway() {
+        return new BalanceGatewayImpl(this.balanceRepository);
     }
 
     @Bean
@@ -39,14 +54,19 @@ public class ApplicationConfig {
 
     @Bean
     @ConditionalOnMissingBean(UseCaseTransaction.class)
-    public UseCaseTransaction useCaseTransaction(final AccountGateway accountGateway, final TransactionGateway transactionGateway) {
-        return new UseCaseTransactionImpl(transactionGateway, accountGateway);
+    public UseCaseTransaction useCaseTransaction(
+        final AccountGateway accountGateway,
+        final TransactionGateway transactionGateway,
+        final BalanceGateway balanceGateway) {
+        return new UseCaseTransactionImpl(transactionGateway, accountGateway, balanceGateway);
     }
 
     @Bean
     @ConditionalOnMissingBean(UseCaseAccount.class)
-    public UseCaseAccount useCaseAccount(final AccountGateway accountGateway) {
-        return new UseCaseAccountImpl(accountGateway);
+    public UseCaseAccount useCaseAccount(
+        final AccountGateway accountGateway,
+        final BalanceGateway balanceGateway) {
+        return new UseCaseAccountImpl(accountGateway, balanceGateway);
     }
 
 }
